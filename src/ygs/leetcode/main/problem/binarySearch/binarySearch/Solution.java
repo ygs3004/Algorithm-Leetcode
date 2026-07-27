df3836049f6adb376ea73503ee6b9507cdc3d6e5
ygs <ygs3004@naver.com>
2026-07-27 23:34:52 +0900

    Binary Search - binary search 주석

--- src/ygs/leetcode/main/problem/binarySearch/binarySearch/Solution.java
+++ src/ygs/leetcode/main/problem/binarySearch/binarySearch/Solution.java
@@ -3,25 +3,36 @@ package ygs.leetcode.main.problem.binarySearch.binarySearch;
 public class Solution {
     public int search(int[] nums, int target) {
         int len = nums.length;
-        int row = 0;
+        int low = 0;
         int high = len - 1;
-        int mid = (row + high) / 2;
+        int mid = (low + high) / 2;
+
+        // while 의 조건문을 통해 타겟에 도달했는지 확인
         while(nums[mid] != target){
             int cur = nums[mid];
             if(cur > target){
+                // index 범위를 초과하거나, 답이 없거나를 체크
                 if(mid - 1 < 0 || nums[mid - 1] < target){
                     mid = -1;
                     break;
                 }
+                
+                // 중간점 체크를 위해 값 비교를 통해, low - high 값 전환
                 high = mid - 1;
+
             }else{
+                // index 범위를 초과하거나, 답이 없거나를 체크
                 if(mid + 1 >= nums.length || nums[mid + 1] > target){
                     mid = -1;
                     break;
                 }
-                row = mid + 1;
+
+                // 중간점 체크를 위해 값 비교를 통해, low - high 값 전환
+                low = mid + 1;
             }
-            mid = (high + row) / 2;
+            
+            // 변경된 low, high 값을 이용해 새로운 중간점 변경
+            mid = (high + low) / 2;
         }
 
         return mid;
