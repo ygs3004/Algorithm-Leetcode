da78d68f7429aac0e28cfe8c9c3b5961e2fc1f7f
ygs <ygs3004@naver.com>
2026-08-10 18:47:57 +0900

    Trie - Implement Trie

---
+++ src/ygs/leetcode/main/problem/trie/implementTrie/Trie.java
@@ -0,0 +1,38 @@
+package ygs.leetcode.main.problem.trie.implementTrie;
+
+import java.util.*;
+
+public class Trie {
+
+    List<String> list;
+
+    public Trie() {
+        list = new ArrayList<>();
+    }
+
+    public void insert(String word) {
+        list.add(word);
+    }
+
+    public boolean search(String word) {
+        return list.contains(word);
+    }
+
+    public boolean startsWith(String prefix) {
+        for(int i = 0; i < list.size(); i++){
+            String str = list.get(i);
+            if(str.length() >= prefix.length()
+                    && str.substring(0, prefix.length()).equals(prefix)) return true;
+        }
+
+        return false;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
